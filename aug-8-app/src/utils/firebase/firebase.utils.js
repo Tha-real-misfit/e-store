3b21a8d40c4be20952f9249004a477d2e63f6326
Chapman <2067198@cognizant.com>
2022-08-16 10:04:27 +0930

    updating log system

--- aug-8-app/src/utils/firebase/firebase.utils.js
+++ aug-8-app/src/utils/firebase/firebase.utils.js
@@ -6,6 +6,8 @@ import {
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
 } from 'firebase/auth';
 import {
   getFirestore,
@@ -83,9 +85,10 @@ export const signInAuthUserWithEmailAndPassword = async (
 ) => {
   if (!email || !password) return;
 
-  return await signInAuthUserWithEmailAndPassword(
-    auth,
-    email,
-    password
-  );
+  return await signInWithEmailAndPassword(auth, email, password);
 };
+
+export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback) =>
+  onAuthStateChanged(auth, callback);
